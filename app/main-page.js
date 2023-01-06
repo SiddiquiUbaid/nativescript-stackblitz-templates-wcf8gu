141d6653a97726f1c78672add8259195c6eeef5c
SiddiquiUbaid <73527547+SiddiquiUbaid@users.noreply.github.com>
2023-01-06 15:13:32 +0530

    2 files modified

--- app/main-page.js
+++ app/main-page.js
@@ -1,6 +1,15 @@
+const frameModule = require('ui/frame');
+
 function onTap(args) {
   const button = args.object;
   const page = button.page;
-  page.frame.navigate("home");
+
+  frameModule.topmost().navigate({
+    moduleName: 'home/home',
+    clearHistory: true,
+  });
+
+  // console.log('trying to open home page ' + page.frame.navigate('home/home'));
+  // page.frame.navigate('main-page');
 }
 exports.onTap = onTap;
